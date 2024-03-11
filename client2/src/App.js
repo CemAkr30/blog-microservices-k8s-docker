64c8d174812d9f07dcc32b3480cd8ed4e3a70186
CemAkr30 <cemakar18@gmail.com>
2024-03-11 11:15:30 +0300

    client push

---
+++ client2/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PostCreate from "./PostCreate.js";
+import PostList from "./PostList.js";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>Create Post!!!!</h1>
+        <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ client2/src/CommentList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const CommentList = ({ comments }) => {
+  const renderedComments = Object.values(comments).map((comment) => {
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+  return (
+    <>
+      <div>
+        <p>{renderedComments.length} comments</p>
+        <ul>{renderedComments}</ul>
+      </div>
+    </>
+  );
+};
+
+export default CommentList;
